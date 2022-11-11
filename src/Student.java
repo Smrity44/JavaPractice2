cc9db2e251474d6b5574a2cfd8969c4e6b0ac30e
Smrity44 <Smritybdny@gmail.com>
2022-11-10 23:44:30 -0500

    adding new test method

--- src/Student.java
+++ src/Student.java
@@ -20,6 +20,10 @@ public class Student {
         course = "QA";
     }
 
+    public void testMethod(){
+        System.out.println("This is the test Method");
+    }
+
     public static void main(String[] args){
         Student student1= new Student();
         Student student2= new Student();
